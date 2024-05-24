11b7580bf837a4958463faa305075043439555e4
YakovlevDanylo <yakovlevdv31@gmail.com>
2024-05-24 13:10:16 +0300

    Add code to check all my exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String[][] validMatrix = {
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+        };
+
+        String[][] invalidSizeMatrix = {
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+        };
+
+        String[][] invalidDataMatrix = {
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "three"},
+                {"1", "2", "3", "4"},
+        };
+
+        try {
+            ArrayValueCalculator.doCalc(validMatrix);
+        } catch (ArrayDataException | ArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ArrayValueCalculator.doCalc(invalidSizeMatrix);
+        } catch (ArrayDataException | ArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ArrayValueCalculator.doCalc(invalidDataMatrix);
+        } catch (ArrayDataException | ArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
